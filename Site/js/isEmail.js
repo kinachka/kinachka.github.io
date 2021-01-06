913c3a495c6a50dcf7e66b45917709b83e3fba78
kinachka <71414507+kinachka@users.noreply.github.com>
2021-01-06 07:34:48 +0300

    Delete Site/js/isEmail.js

--- Site/js/isEmail.js
+++
@@ -1,8 +0,0 @@
-function isEmail() {
-    var str = document.getElementById("email").value;
-    if (isEmpty(str)) status.innerHTML = alert('ты ниче не ввел');
-    else alert(str);
-  }
-  function isEmpty(str) {
-    return str == null || str.length == 0;
-  }
