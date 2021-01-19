24e4fe0f6ac1393ec8e07ab639385a414a00aba3
kinachka <tensenewera@bk.ru>
2021-01-19 10:32:38 +0300

    123

---
+++ Site/js/main.js
@@ -0,0 +1,18 @@
+$(window).load(function () {
+  $("#preloader-wrapper").fadeOut("slow");
+});
+
+$(document).ready(function () {
+  //animated header class
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    //console.log(scroll);
+    if (scroll > 200) {
+      //console.log('a');
+      $(".navigation").addClass("animated");
+    } else {
+      //console.log('a');
+      $(".navigation").removeClass("animated");
+    }
+  });
+});
