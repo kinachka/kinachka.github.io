ec6dcc74ff30d69c5c1b8b8cbc1e1d8f5d61f42f
Artem Parfenov <tensenewera@bk.ru>
2022-05-24 22:43:19 +0300

    poecalc add

---
+++ script.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let num = prompt();
+
+let arr = [];
+
+for (let i = 1; i < 10; i++) {
+    arr[i] =  (0.1 * i * num).toFixed(); 
+}
+console.log(arr);
+console.log(num);
+document.write("/autoreply ex: ",num,", 0,1: ",arr[1],", 0,2: ",arr[2],", 0,3: ",arr[3],", 0,4: ",arr[4],", 0,5: ",arr[5],", 0,6: ",arr[6],", 0,7: ",arr[7],", 0,8: ",arr[8],", 0,9: ",arr[9])
