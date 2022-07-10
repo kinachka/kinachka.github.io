d238a5a2992c23d6924c7cb33fc154fa6ad93e37
Artem Parfenov <tensenewera@bk.ru>
2022-07-10 12:32:07 +0300

    emojis

--- weatherandforecast/js/forecast.js
+++ weatherandforecast/js/forecast.js
@@ -4,6 +4,8 @@ const api_key = '&appid=2d8efa497b04dd86220fd265458c6bcc'
 const units = '&units=metric'
 const api_lang = '&lang=ru'
 
+
+
 async function getData() {
 	let api_url = url + cityname + api_key + units + api_lang
 	const response = await fetch (api_url)
@@ -11,13 +13,22 @@ async function getData() {
 	console.log(data.city.name)
 	console.log(data.list[0].main.temp)
 	console.log(data)
+	let emojivalue
 	document.getElementById('name').textContent = data.city.name
-
+	
 	for (let i = 1; i < 6; i++) {
-		document.getElementById(`temp${i}`).textContent = data.list[i].main.temp
-		document.getElementById(`desc${i}`).textContent = data.list[i].weather[0].description
+		if (data.list[i].weather[0].description == 'пасмурно') {emojivalue = ' ☁️'}
+		if (data.list[i].weather[0].description == 'облачно с прояснениями') {emojivalue = ' 🌤️'}
+		if (data.list[i].weather[0].description == 'небольшой дождь') {emojivalue = ' 🌧️'}
+		if (data.list[i].weather[0].description == 'дождь') {emojivalue = ' 🌧️'}
+		if (data.list[i].weather[0].description == 'переменная облачность') {emojivalue = ' ⛅'}
+		if (data.list[i].weather[0].description == 'ясно') {emojivalue = ' ☀️'}
+		if (data.list[i].weather[0].description == 'небольшая облачность') {emojivalue = ' 🌤️'}
+		document.getElementById(`temp${i}`).textContent = data.list[i].main.temp 
+		document.getElementById(`desc${i}`).textContent = data.list[i].weather[0].description + emojivalue
 		document.getElementById(`date${i}`).textContent = data.list[i].dt_txt
 	}
+	
 }
 getData()
 
@@ -25,3 +36,6 @@ document.getElementById("citySubmitButton").onclick = function() {
 	cityname = document.getElementById('citynameinput').value
 	getData()
 }
+
+
+	
--- weatherandforecast/js/script.js
+++ weatherandforecast/js/script.js
@@ -3,6 +3,7 @@ let cityname = 'Petrozavodsk'
 const api_key = '&appid=2d8efa497b04dd86220fd265458c6bcc'
 const units = '&units=metric'
 const api_lang = '&lang=ru'
+let emojivalue
 
 async function getData() {
 	let api_url = url + cityname + api_key + units + api_lang
@@ -11,9 +12,16 @@ async function getData() {
 	console.log(data.name)
 	console.log(data.main.temp)
 	console.log(data.weather[0].description)
+	if (data.weather[0].description == 'пасмурно') {emojivalue = ' ☁️'}
+	if (data.weather[0].description == 'облачно с прояснениями') {emojivalue = ' 🌤️'}
+	if (data.weather[0].description == 'небольшой дождь') {emojivalue = ' 🌧️'}
+	if (data.weather[0].description == 'дождь') {emojivalue = ' 🌧️'}
+	if (data.weather[0].description == 'переменная облачность') {emojivalue = ' ⛅'}
+	if (data.weather[0].description == 'ясно') {emojivalue = ' ☀️'}
+	if (data.weather[0].description == 'небольшая облачность') {emojivalue = ' 🌤️'}
 	document.getElementById('name').textContent = data.name
 	document.getElementById('temp').textContent = data.main.temp
-	document.getElementById('desc').textContent = data.weather[0].description
+	document.getElementById('desc').textContent = data.weather[0].description + emojivalue
 }
 getData()
 
