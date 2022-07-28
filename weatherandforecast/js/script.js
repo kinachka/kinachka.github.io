89900dd6468aef2af22d7056b697b989b2a97e85
Artem Parfenov <tensenewera@bk.ru>
2022-07-28 18:01:38 +0300

    Update script.js

--- weatherandforecast/js/script.js
+++ weatherandforecast/js/script.js
@@ -9,17 +9,17 @@ async function getData() {
 	let api_url = url + cityname + api_key + units + api_lang
 	const response = await fetch (api_url)
 	const data = await response.json()
-	console.log(data.name)
-	console.log(data.main.temp)
-	console.log(data.weather[0].description)
-	if (data.weather[0].description == 'пасмурно') {emojivalue = ' ☁️'}
-	if (data.weather[0].description == 'облачно с прояснениями') {emojivalue = ' 🌤️'}
-	if (data.weather[0].description == 'небольшой дождь') {emojivalue = ' 🌧️'}
-	if (data.weather[0].description == 'дождь') {emojivalue = ' 🌧️'}
-	if (data.weather[0].description == 'переменная облачность') {emojivalue = ' ⛅'}
-	if (data.weather[0].description == 'ясно') {emojivalue = ' ☀️'}
-	if (data.weather[0].description == 'небольшая облачность') {emojivalue = ' 🌤️'}
-	if (data.weather[0].description == 'небольшая морось') {emojivalue = ' 🌧️'}
+	const varForEmoji = data.weather[0].description
+	if (varForEmoji == 'пасмурно') {emojivalue = ' ☁️'}
+	if (varForEmoji == 'облачно с прояснениями') {emojivalue = ' 🌤️'}
+	if (varForEmoji == 'небольшой дождь') {emojivalue = ' 🌧️'}
+	if (varForEmoji == 'дождь') {emojivalue = ' 🌧️'}
+	if (varForEmoji == 'переменная облачность') {emojivalue = ' ⛅'}
+	if (varForEmoji == 'ясно') {emojivalue = ' ☀️'}
+	if (varForEmoji == 'небольшая облачность') {emojivalue = ' 🌤️'}
+	if (varForEmoji == 'небольшая морось') {emojivalue = ' 🌧️'}
+	if (varForEmoji == 'небольшой проливной дождь') {emojivalue = ' 🌧️'}
+	
 	document.getElementById('name').textContent = data.name
 	document.getElementById('temp').textContent = data.main.temp
 	document.getElementById('desc').textContent = data.weather[0].description + emojivalue
